fd49f18b5b0a6c31beba9aeebfea9277b7bfba9e
zakariya sakho <zakariyasakho9@gmail.com>
2026-06-28 22:17:39 +0200

    JS : compteurs animés au scroll et animations fade-in des sections

---
+++ js/main.js
@@ -0,0 +1,40 @@
+const btn = document.getElementById("theme-btn");
+
+btn.addEventListener("click", () => {
+    document.body.classList.toggle("dark-mode");
+
+    if (document.body.classList.contains("dark-mode")) {
+        localStorage.setItem("theme", "dark");
+    } else {
+        localStorage.setItem("theme", "light");
+    }
+});
+
+if (localStorage.getItem("theme") === "dark") {
+    document.body.classList.add("dark-mode");
+}
+window.addEventListener("scroll", () => {
+    const nav = document.querySelector(".navbar");
+
+    if (window.scrollY > 50) {
+        nav.classList.add("scrolled");
+    } else {
+        nav.classList.remove("scrolled");
+    }
+});
+const topBtn = document.getElementById("topBtn");
+
+window.addEventListener("scroll", () => {
+    if (window.scrollY > 300) {
+        topBtn.style.display = "block";
+    } else {
+        topBtn.style.display = "none";
+    }
+});
+
+topBtn.addEventListener("click", () => {
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    });
+});
